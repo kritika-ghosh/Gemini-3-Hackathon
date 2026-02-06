63f43cd3dd4c7aae7df088ac6e5c82e3dcb1ac18
kritika-ghosh <kri.kritika.ghosh@gmail.com>
2026-02-06 21:10:32 +0530

    Delete agents directory

--- agents/__init__.py
+++
@@ -1,9 +0,0 @@
-# agents/__init__.py
-
-# Relative imports to expose classes at the package level
-from .architect import ArchitectAgent
-from .librarian import LibrarianAgent
-from .multimodal import MultimodalSpecialist
-
-# Define __all__ to control what is exported during 'from agents import *'
-__all__ = ["ArchitectAgent", "LibrarianAgent", "MultimodalSpecialist"]
